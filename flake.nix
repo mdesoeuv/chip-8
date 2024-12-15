5f8763178efd6f51a334bcd7d441d6ed46b67536
DoomDuck <docdoomduck@protonmail.com>
2024-12-15 11:40:05 +0100

    feat(flake): audio support, comment and format

--- flake.nix
+++ flake.nix
@@ -1,24 +1,60 @@
 {
-  outputs = { nixpkgs, ... }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    buildInputs = with pkgs; [
-      # Keyboard input
-      libxkbcommon
-      # WINIT_UNIX_BACKEND=wayland
-      wayland
+  description = "CHIP-8 emulator";
 
-      # Vulkan
-      vulkan-headers
-      vulkan-loader
-    ];
-    in {
+  outputs =
+    { nixpkgs, ... }:
+    let
+      # Supported target
+      system = "x86_64-linux";
+
+      # Package set used
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # Building tools
+      nativeBuildInputs = with pkgs; [
+        # Package config
+        pkg-config
+
+        # Add support for mold the super fast linker
+        mold
+        # It requires the presence of clang to work
+        clang
+      ];
+
+      # Libraries
+      buildInputs = with pkgs; [
+        # Keyboard input
+        libxkbcommon
+
+        # Wayland windowing protocol
+        wayland
+
+        # Vulkan graphics
+        vulkan-headers
+        vulkan-loader
+
+        # Audio
+        alsa-lib
+      ];
+
+    in
+    {
+      # Formatter
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      # Devellopement shell accessible with `nix develop`
       devShells.${system}.default = pkgs.mkShell {
         name = "chip-8";
+
+        inherit nativeBuildInputs;
+
         inherit buildInputs;
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}
-        '';
+
+        # Add dynamic libraries to loading path
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+
+        # Add support to various higher level audio systems (pulseaudio, jack, ...)
+        ALSA_PLUGIN_DIR = "${pkgs.alsa-plugins}/lib/alsa-lib";
       };
     };
 }
